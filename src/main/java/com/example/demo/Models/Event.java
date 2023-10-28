0fdd1ba7c91ad717dffc78b9d4b8decb33ab34ca
OctavianGheorghiu <127142875+OctavianGheorghiu@users.noreply.github.com>
2023-10-28 13:50:34 +0300

    aded entiies

---
+++ src/main/java/com/example/demo/Models/Event.java
@@ -0,0 +1,100 @@
+package com.example.demo.Models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import org.springframework.stereotype.Component;
+
+@Component
+@Table(name = "event")
+public class Event {
+
+    @Id
+    private long id;
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "location")
+    private String location;
+
+    @Column(name = "date")
+    private String date;
+
+    @Column(name = "points")
+    private int points;
+
+    public Event() {
+    }
+
+    public Event(long id, String title, String description, String location, String date, int points) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.location = location;
+        this.date = date;
+        this.points = points;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public void setPoints(int points) {
+        this.points = points;
+    }
+
+    @Override
+    public String toString() {
+        return "Event{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", location='" + location + '\'' +
+                ", date='" + date + '\'' +
+                ", points='" + points + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/com/example/demo/Models/Person.java
@@ -0,0 +1,99 @@
+package com.example.demo.Models;
+
+import com.fasterxml.jackson.annotation.JsonTypeId;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@Component
+@Table(name = "person")
+public class Person {
+
+    @Id
+    private long id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "email")
+    private String email;
+    @Column(name = "password")
+    private String password;
+    @Column(name = "city")
+    private String city;
+    @Column(name = "points")
+    private int points;
+
+    public Person() {
+    }
+
+    public Person(long id, String name, String email, String password, String city, int points) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.city = city;
+        this.points = points;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public void setPoints(int points) {
+        this.points = points;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", city='" + city + '\'' +
+                ", points=" + points +
+                '}';
+    }
+}
