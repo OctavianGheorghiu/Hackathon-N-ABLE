02bbf5d86360cdeda1ae6083771f7f037dafbf9d
Ignat Stefan <ignatstefan21@stud.ase.ro>
2023-10-28 14:32:31 +0300

    changed repository type

--- src/main/java/com/example/demo/Repositories/EventRepository.java
+++ src/main/java/com/example/demo/Repositories/EventRepository.java
@@ -1,4 +1,12 @@
 package com.example.demo.Repositories;
 
-public interface EventRepository {
+import com.example.demo.Models.Event;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface EventRepository extends JpaRepository<Event, Long> {
+
 }
--- src/main/java/com/example/demo/Repositories/PersonRepository.java
+++ src/main/java/com/example/demo/Repositories/PersonRepository.java
@@ -1,4 +1,12 @@
 package com.example.demo.Repositories;
 
-public interface PersonRepository {
+import com.example.demo.Models.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface PersonRepository extends JpaRepository<Person, Long> {
+
 }
